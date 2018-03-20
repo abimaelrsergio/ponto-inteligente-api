491cd6510cb784e29aecdc769787210cb23f99af
abimaelrsergio <abimaelr.sergio@gmail.com>
2018-03-20 19:25:08 -0300

    autenticacao

---
+++ src/main/java/com/abgi/pontointeligente/api/security/services/JwtUserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.abgi.pontointeligente.api.security.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.abgi.pontointeligente.api.entities.Funcionario;
+import com.abgi.pontointeligente.api.security.JwtUserFactory;
+import com.abgi.pontointeligente.api.services.FuncionarioService;
+
+@Service
+public class JwtUserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private FuncionarioService funcionarioService;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<Funcionario> funcionario = funcionarioService.buscarPorEmail(username);
+
+		if (funcionario.isPresent()) {
+			return JwtUserFactory.create(funcionario.get());
+		}
+
+		throw new UsernameNotFoundException("Email não encontrado.");
+	}
+
+}
